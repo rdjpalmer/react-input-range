fe4535c6ee318ba102d0f4bf50e6d58999c12fac
davidchin <davidchin.yf@gmail.com>
2017-01-23 08:09:11 +1100

    style: Rename example app

--- example/js/example-app.jsx
+++ example/js/example-app.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable class-methods-use-this, no-console */
+
 import React from 'react';
 import InputRange from '../../src/js';
 
-class App extends React.Component {
+export default class ExampleApp extends React.Component {
   constructor(props) {
     super(props);
 
@@ -118,5 +120,3 @@ class App extends React.Component {
     );
   }
 }
-
-export default App;
